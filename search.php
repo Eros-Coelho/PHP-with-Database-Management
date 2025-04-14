465c134b13239109489ce163be5c0b74eacfcf70
Eros Coelho <eros.limacoelho@student.griffith.ie>
2025-04-14 14:32:51 +0100

    added connection to the database and php to the other 3 files

---
+++ search.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$con = mysqli_connect("localhost", "root", "", "assignment2");
+// connecting to the apache server and the databse "assignment2"
+
+if (!$con){
+    echo "Error: Could not connect to Database";
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+}
+
+?>
