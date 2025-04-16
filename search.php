2b2fd5402ac7d2cd4a4ec3f8a74e08f4081765db
Eros Coelho <eros.limacoelho@student.griffith.ie>
2025-04-16 18:28:56 +0100

    finished search.php

--- search.php
+++ search.php
@@ -9,42 +9,27 @@ if (!$con){
     echo "Error: Could not connect to Database";
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['serial-number'])){
 
-    // variables for user table
-    $user_id = $_GET['user-id'];
-    $first_name = $_GET['fName'];
-    $last_name = $_GET['lName'];
-    $address = $_GET['address'];
-    $eir_code = $_GET['eir-code'];
-    $phone = $_GET['phone'];
-    $email = $_GET['email'];
-
-    // variables for appliance table
-    $appliance_id = $_GET['appliance-id'];
-    $brand = $_GET['brand'];
-    $model = $_GET['model'];
     $serial_number = $_GET['serial-number'];
-    $purchase_date = $_GET['purchase-date'];
-    $warranty_expiration = $_GET['warranty-expiration'];
-    $appliance_cost = $_GET['appliance-cost'];
-    $appliance_type = $_GET['appliance-type'];   
-
+    
     $querySearch = "SELECT * FROM Appliances WHERE serial_number = '$serial_number'";
     $resultSearch = mysqli_query($con, $querySearch);
 
     if ($resultSearch -> num_rows > 0){
 
         $appliance_data = $resultSearch -> fetch_assoc();
-        echo "Appliance ID:".$appliance_data['appliance-id'];
-        echo "Brand:".$appliance_data['brand'];
-        echo "Model:".$appliance_data['model'];
-        echo "Serial Number:".$appliance_data['serial-number'];
-        echo "Purchase Date:".$appliance_data['purchase-date'];
-        echo "Warranty Expiration Date:".$appliance_data['warranty-expiration'];
-        echo "Cost: $".$appliance_data['appliance-cost'];
-        echo "Type:".$appliance_data['appliance-type'];
-        echo "Owner:".$appliance_data['user-id'];
+
+        echo "<h3>Appliance Details:</h3>";
+        echo "Appliance ID: " . $appliance_data['applianceID'] . "<br>";
+        echo "Brand: " . $appliance_data['brand'] . "<br>";
+        echo "Model: " . $appliance_data['model'] . "<br>";
+        echo "Serial Number: " . $appliance_data['serial_number'] . "<br>";
+        echo "Purchase Date: " . $appliance_data['purchase_date'] . "<br>";
+        echo "Warranty Expiration Date: " . $appliance_data['warranty_expiration'] . "<br>";
+        echo "Cost: €" . $appliance_data['appliance_cost'] . "<br>";
+        echo "Type: " . $appliance_data['appliance_type'] . "<br>";
+        echo "Owner ID: " . $appliance_data['userID'] . "<br>";
 
     } else {
         echo "Sorry, no appliance with that serial number was found";
@@ -64,7 +49,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET'){
             <h3>Please enter the serial number of the appliance you are looking for:</h3>
 
             <label for="serial-number">Serial Number:</label>
-    <input type="text" name="serial-number" value="<?php echo isset($_GET['serial-number']) ? $_GET['serial-number'] : ''; ?>"></br>
+    <input type="text" name="serial-number" value="<?php echo isset($_GET['serial-number']) ? $_GET['serial-number'] : ''; ?>">
+
+    <button type="submit">Search</button></br>
         
     <a href="index.html">Return Home</a>
     </body>
